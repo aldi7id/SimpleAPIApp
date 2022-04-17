9062bceb17ee46362c981d235ccbc6e698ec37fe
aldi7id <aldi7.indo@gmail.com>
2022-04-17 13:39:55 +0700

    setting home fragment

--- app/src/main/java/com/ajgroup/themoviedbapp/home/HomeFragment.kt
+++ app/src/main/java/com/ajgroup/themoviedbapp/home/HomeFragment.kt
@@ -2,31 +2,75 @@ package com.ajgroup.themoviedbapp.home
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ajgroup.themoviedbapp.R
+import com.ajgroup.themoviedbapp.database.RegisterDatabase
+import com.ajgroup.themoviedbapp.database.RegisterRepository
+import com.ajgroup.themoviedbapp.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
-
-    private lateinit var viewModel: HomeViewModel
-
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: HomeFragmentBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+        _binding = HomeFragmentBinding.inflate(inflater,container,false)
+
+
+        val application = requireNotNull(this.activity).application
+
+        val dao = RegisterDatabase.getInstance(application).registerDatabaseDao
+
+        val repository = RegisterRepository(dao)
+
+        val factory = HomeViewModelFactory(repository, application)
+
+        homeViewModel =
+            ViewModelProvider(this, factory).get(HomeViewModel::class.java)
+
+        binding.homeLayout = homeViewModel
+
+        binding.lifecycleOwner = this
+
+        homeViewModel.navigateto.observe(viewLifecycleOwner, Observer { hasFinished ->
+            if (hasFinished == true) {
+                val action = HomeFragmentDirections.actionHomeFragmentToProfileFragment()
+                NavHostFragment.findNavController(this).navigate(action)
+                homeViewModel.doneNavigating()
+            }
+        })
+
+        initRecyclerView()
+
+        return binding.root
+
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+
+    private fun initRecyclerView() {
+        binding.usersRecyclerView.layoutManager = LinearLayoutManager(this.context)
+        displayUsersList()
+    }
+
+
+    private fun displayUsersList() {
+        Log.i("MYTAG", "Inside ...UserDetails..Fragment")
+        homeViewModel.users.observe(viewLifecycleOwner, Observer {
+            binding.usersRecyclerView.adapter = MyRecycleViewAdapter(it)
+        })
+
     }
 
 }
--- app/src/main/java/com/ajgroup/themoviedbapp/home/HomeViewModel.kt
+++ app/src/main/java/com/ajgroup/themoviedbapp/home/HomeViewModel.kt
@@ -1,7 +1,36 @@
 package com.ajgroup.themoviedbapp.home
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ajgroup.themoviedbapp.database.RegisterRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel (private val repository: RegisterRepository, application: Application):AndroidViewModel(application){
+
+    val users = repository.users
+    init {
+        Log.i("MYTAG","inside_users_Lisrt_init")
+    }
+
+
+    private val _navigateto = MutableLiveData<Boolean>()
+
+    val navigateto: LiveData<Boolean>
+        get() = _navigateto
+
+    fun doneNavigating(){
+        _navigateto.value=false
+    }
+
+    fun backButtonclicked(){
+        _navigateto.value = true
+    }
+
 }
