a119f1df7718528c2c8296ea4ecc7b9710adc8f6
aldi7id <aldi7.indo@gmail.com>
2022-04-19 23:50:02 +0700

    profilefragment (belum fix)

--- app/src/main/java/com/ajgroup/themoviedbapp/profile/ProfileFragment.kt
+++ app/src/main/java/com/ajgroup/themoviedbapp/profile/ProfileFragment.kt
@@ -2,14 +2,21 @@ package com.ajgroup.themoviedbapp.profile
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.ajgroup.themoviedbapp.R
+import com.ajgroup.themoviedbapp.database.RegisterDatabase
+import com.ajgroup.themoviedbapp.database.RegisterRepository
+import com.ajgroup.themoviedbapp.databinding.ProfileFragmentBinding
 
 class ProfileFragment : Fragment() {
-
+    private lateinit var profileViewModel: ProfileViewModel
+    private var _binding: ProfileFragmentBinding? = null
+    private val binding get() = _binding!!
     companion object {
         fun newInstance() = ProfileFragment()
     }
@@ -20,7 +27,23 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+        _binding = ProfileFragmentBinding.inflate(inflater,container,false)
+        val application = requireNotNull(this.activity).application
+
+        val dao = RegisterDatabase.getInstance(application).registerDatabaseDao
+
+        val repository = RegisterRepository(dao)
+
+        val factory = ProfileViewModelFactory(repository,application)
+        profileViewModel = ViewModelProvider(this, factory).get(ProfileViewModel::class.java)
+
+        binding.myViewModel = profileViewModel
+        binding.lifecycleOwner = this
+
+        profileViewModel.userDetailsLiveData.observe(viewLifecycleOwner, Observer {
+            Log.i("MYTAG",it.toString()+"000000000000000000000000")
+        })
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
