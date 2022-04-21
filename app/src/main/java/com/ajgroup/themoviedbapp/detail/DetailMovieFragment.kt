985a25e702ed4686848300a7bd6e63d5da3b8959
aldi7id <aldi7.indo@gmail.com>
2022-04-21 20:59:10 +0700

    getstring

--- app/src/main/java/com/ajgroup/themoviedbapp/detail/DetailMovieFragment.kt
+++ app/src/main/java/com/ajgroup/themoviedbapp/detail/DetailMovieFragment.kt
@@ -10,8 +10,6 @@ import android.widget.Toast
 import androidx.navigation.fragment.navArgs
 import com.ajgroup.themoviedbapp.R
 import com.ajgroup.themoviedbapp.databinding.DetailMovieFragmentBinding
-import com.ajgroup.themoviedbapp.databinding.HomeFragmentBinding
-import com.ajgroup.themoviedbapp.home.HomeViewModel
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.detail_movie_fragment.*
 
@@ -21,15 +19,11 @@ class DetailMovieFragment : Fragment() {
     private val binding get() = _binding!!
     private val IMAGE_BASE ="https://image.tmdb.org/t/p/w500/"
     val args: DetailMovieFragmentArgs by navArgs()
-    companion object {
-        fun newInstance() = DetailMovieFragment()
-    }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = DetailMovieFragmentBinding.inflate(inflater,container,false)
 
         return binding.root
@@ -42,12 +36,12 @@ class DetailMovieFragment : Fragment() {
 
         detailMovieViewModel.detailMovie.observe(viewLifecycleOwner){
             binding.apply {
-                tvJudul.text = "Tittle:" + it.originalTitle
-                tvGenre.text = "Genre: " + it.genres[0].name + " or " + it.genres[1].name
-                tvRelease.text = "Release Date: " + it.releaseDate
-                tvTagLine.text = "Tagline: " + it.tagline
-                tvStatus.text = "Status: " + it.status
-                tvDesc.text = "Description:" +it.overview
+                tvJudul.text = getString(R.string.tittless).plus(it.originalTitle)
+                tvGenre.text = getString(R.string.genre).plus(it.genres[0].name ).plus(getString(R.string.or)).plus(it.genres[1].name)
+                tvRelease.text = getString(R.string.release).plus(it.releaseDate)
+                tvTagLine.text = getString(R.string.tagline).plus(it.tagline)
+                tvStatus.text = getString(R.string.status).plus(it.status)
+                tvDesc.text = getString(R.string.Desc).plus(it.overview)
             }
             Glide.with(binding.root).load(IMAGE_BASE+it.backdropPath).into(iv_header)
             Toast.makeText(context, "Success Load Detail", Toast.LENGTH_SHORT).show()
